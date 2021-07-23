681bab3224a7d4bc2791d1fe231b9ec10705f693
sheylaPozo <shiggipoke@gmail.com>
2021-07-23 13:22:39 -0500

    add status.js , dragdropjs add listener, and drag and drop function, add activities

--- src/TodoBottomDiv.js
+++
@@ -1,7 +0,0 @@
-export default function clearButton() {
-  const clearButton = document.createElement('button');
-  clearButton.classList.add('clear-button');
-  clearButton.type = 'submit';
-  clearButton.innerHTML = 'Clear all completed';
-  return clearButton;
-}
